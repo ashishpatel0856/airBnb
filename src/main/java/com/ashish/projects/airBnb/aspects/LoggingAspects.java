ebef76aa0c1deec0b5ca2af4a2467a3c650379ed
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-10-29 13:58:42 +0530

    aop

--- src/main/java/com/ashish/projects/airBnb/aspects/LoggingAspects.java
+++ src/main/java/com/ashish/projects/airBnb/aspects/LoggingAspects.java
@@ -1,5 +1,6 @@
 package com.ashish.projects.airBnb.aspects;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -9,10 +10,10 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class LoggingAspects {
-
-    private static final Logger log = LoggerFactory.getLogger(LoggingAspects.class);
-
+    //before cross cutting concerns
+    
     @Before("execution(* com.ashish.projects.airBnb.service.*.*(..))")// all serivce ke api ko chalane pr ye phle console me print ho jayega
     public void debugProxy(JoinPoint joinPoint) {
         log.info("Proxy target: {}", joinPoint.getTarget().getClass());
