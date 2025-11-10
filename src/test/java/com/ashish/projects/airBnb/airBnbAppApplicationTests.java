50180107bfdbd71f84080e2f617b9a50e2dba3e5
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-11-10 18:59:50 +0530

    cifile

--- src/test/java/com/ashish/projects/airBnb/airBnbAppApplicationTests.java
+++ src/test/java/com/ashish/projects/airBnb/airBnbAppApplicationTests.java
@@ -3,7 +3,7 @@ package com.ashish.projects.airBnb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class airBnbAppApplicationTests {
 
 	@Test
