45c1d67668e54e952f8777745fafb941c293e847
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-11-10 18:46:18 +0530

    final

--- src/main/java/com/ashish/projects/airBnb/airBnbAppApplication.java
+++ src/main/java/com/ashish/projects/airBnb/airBnbAppApplication.java
@@ -1,14 +1,10 @@
 package com.ashish.projects.airBnb;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(proxyTargetClass = true)
-
 public class airBnbAppApplication {
 
 	public static void main(String[] args) {
