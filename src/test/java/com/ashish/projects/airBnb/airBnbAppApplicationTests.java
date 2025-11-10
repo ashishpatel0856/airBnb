af19c7c456899f8a00c100d6782c59f3363a570a
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-11-11 01:15:04 +0530

    text.property

--- src/test/java/com/ashish/projects/airBnb/airBnbAppApplicationTests.java
+++ src/test/java/com/ashish/projects/airBnb/airBnbAppApplicationTests.java
@@ -6,10 +6,9 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class airBnbAppApplicationTests {
+class AirBnbAppApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
