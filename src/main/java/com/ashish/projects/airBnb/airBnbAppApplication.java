6b871da1671d163a67a0292a5c6b6bb5af6f2aed
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-11-11 00:41:49 +0530

    commit

--- src/main/java/com/ashish/projects/airBnb/airBnbAppApplication.java
+++ src/main/java/com/ashish/projects/airBnb/airBnbAppApplication.java
@@ -2,7 +2,6 @@ package com.ashish.projects.airBnb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 public class airBnbAppApplication {
